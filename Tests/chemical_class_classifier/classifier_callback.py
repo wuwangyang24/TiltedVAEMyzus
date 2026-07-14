661bd5744a2539116112b19752f1af638dc49044
Wu Wang Yang <wuwangyang24@gmail.com>
2026-07-14 17:59:01 +0200

    c

--- Tests/chemical_class_classifier/classifier_callback.py
+++ Tests/chemical_class_classifier/classifier_callback.py
@@ -24,6 +24,7 @@ import pandas as pd
 import torch
 import torchvision.transforms as T
 from torchvision.io import ImageReadMode, read_image
+from tqdm import tqdm
 
 import pytorch_lightning as pl
 
@@ -96,7 +97,7 @@ def _encode_all_compounds(
     mode = ImageReadMode.GRAY if in_channels == 1 else ImageReadMode.RGB
 
     embeddings = {}
-    for entry in metadata:
+    for entry in tqdm(metadata, desc="  [ClassifierCallback] Encoding compounds", leave=False):
         compound_id = str(entry["Compound"])
         plate_dict = {}
         for plate_id, plate_data in entry.items():
